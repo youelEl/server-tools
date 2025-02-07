63b1b367d2a468e0d829df75c5ad91ca4e82265e
youel <younes.elahrach@smile-maroc.com>
2025-02-07 09:56:24 +0100

    [MIG] scheduler_error_mailer: Migration to 18.0

--- scheduler_error_mailer/hooks.py
+++ scheduler_error_mailer/hooks.py
@@ -4,7 +4,6 @@ from odoo import SUPERUSER_ID, api
 
 
 def post_init_hook(env):
-    env = api.Environment(env.cr, SUPERUSER_ID, {})
     env["ir.cron"].with_context(active_test=False).search([]).write(
         {
             "email_template_id": env.ref(
